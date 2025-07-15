fb25ce7df78442525ad8eeeb98ce7b8592c11435
Ravindu Sandeep <sandeepravindu112@gmail.com>
2025-07-15 08:25:52 +0530

    add database

--- backend/app/main.py
+++ backend/app/main.py
@@ -20,3 +20,20 @@ def read_root():
 def classify_music(track_url: str):
     # This is a placeholder. In the future, you'd integrate audio classification logic here
     return {"track": track_url, "genre": "Pop", "mood": "Happy"}
+
+######################################################################
+
+
+from pymongo.mongo_client import MongoClient
+
+uri = "mongodb+srv://sandeepravindu112:eoJcNVzzO2UHJKZ7@cluster0.ltmrd93.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+# Create a new client and connect to the server
+client = MongoClient(uri)
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
